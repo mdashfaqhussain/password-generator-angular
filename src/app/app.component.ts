783f5726b4f2c51e92ac876f07774ee3516ef27f
Mohammed Ashfaq <mdashfaqhussain510@gmail.com>
2023-11-02 16:49:38 +0530

    password generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Target } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,62 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'lab-angular';
+
+  length:number = 0;
+  useLetter:Boolean =  false;
+  useNumber:Boolean = false;
+  useSymbol:Boolean = false;
+  password: String= ''
+
+  onChangeLength(event:Event) {
+   const parsedValue= parseInt((event.target as HTMLInputElement).value);
+   if( !isNaN(parsedValue)){
+    this.length = parsedValue;
+   }
+
+  }
+  onChangeUseLetter() {
+    this.useLetter = !this.useLetter;
+    //console.log(this.useLetter);
+  }
+
+  onChangeUsenumber() {
+    this.useNumber = !this.useNumber;
+   // console.log(this.useNumber);
+  }
+
+  onChangeUseSymbols() {
+    this.useSymbol = !this.useSymbol;
+    //console.log(this.useSymbol);
+
+  }
+  onButtonClick() {
+
+    const numbers = '1234567890';
+    const letters= 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = `!@#$%^&*()`
+    let validChars = '';
+    if(this.useLetter) {
+      validChars+=letters;
+    }
+    if(this.useNumber) {
+      validChars+=numbers;
+    }
+    if(this.useSymbol) {
+      validChars+=symbols;
+    }
+
+    let generatedPassword = '';
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length)
+      //console.log(validChars[index]);
+
+      generatedPassword+=validChars[index];
+    }
+    this.password = generatedPassword;
+  }
+
+  getPassword() {
+    return this.password;
+  }
 }
